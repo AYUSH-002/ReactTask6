da2487c0fc1562ce19bebecd5c4dabd4ce5b7fd8
Ayush Sharma <022003ayush@gmail.com>
2023-10-14 21:22:49 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"https://ayush-002.github.io/MyFirstWebsite/",
+    "name": "Ayush Sharma",
+    "role": "Full Stack Developer and Data Analyst",
+    "linkedinId":"https://www.linkedin.com/in/ayush-sharma-47212b26b/",
+    "skypeid": "022003ayush@gmail.com",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ayush-sharma-47212b26b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/AYUSH-002",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/AyushSh38243888",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Vellore Institute of Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"022003ayush@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The Vellore Insitute of Technology",
+        "specialization":"Computer Science in Specilization With Data Science",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2025",
+        "Achievements":"Merit Certificate Holder"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Indian Society for Technical Education",
+        "specialization":"Core committee member",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2023",
+        "Achievements":"Senior Web Developer"
+      },
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Expressjs"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Web3"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project2",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project3",
+        "description":"mobileapp",  
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project4",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Your designing skills are truly impressive and bring a unique and creative touch to everything you create.",
+        "name":"Aditya Kashyap"
+      },
+      {
+        "description":"Your web development skills are impressive and consistently deliver outstanding results.",
+        "name":"Samarth Nag"
+      }
+    ]
+  }
+  
+  export default resumeData
